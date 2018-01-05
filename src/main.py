6bcad730749134490134d6c4ac40e5a304f0ed7e
ribanez <mail@ribanez.cl>
2018-01-05 20:17:57 -0300

    load and write in batches

--- src/main.py
+++ src/main.py
@@ -1,51 +1,22 @@
-from reclamos import *
 from utils import *
 
 
 def main():
-    f = open('../../urls_reclamos.csv')
+    file_urls = '../../urls_reclamos.csv'
 
-    path_out_csv = '../data/reclamos_raw.json'
+    path_out = '../data/reclamos_raw.json'
+    n_batch = 84
 
-    for line in f:
-        line = line.replace('"', '')
-        url = line.replace('\n','')
-
-        try:
-            resp = urllib.request.urlopen(url).read()
-            soup = BeautifulSoup(resp, 'html.parser')
-        except urllib.error.HTTPError as err:
-            error = 'Error {} , url {}'.format(err.code, url)
-            write_error(error)
-            soup = None
-        except ValueError:
-            error = 'Error: Invalid URL {}'.format(url)
-            write_error(error)
-            soup = None
-        
-        if soup != None:
-            title = getTitle(soup, url)
-            description = getDescription(soup, url)
-            keywords = getKeywords(soup, url)
-            itemreview = getItemreview(soup, url)
-            summary = getSummary(soup, url)
-            date_reclamo = getDate_reclamo(soup, url)
-            #date_consulta = getDate_consulta(soup, url)
-            ip_info = getIP_info(soup, url)
-            state_rec = getState_rec(soup, url)
-            reclamo = getReclamo(soup, url)
-            ip_user = getIP_user(soup, url)
-            visitas = getVisitas(soup, url)
-            campo_empresa = getCampoEmpresa(soup, url)
-            empresa = getEmpresa(soup, url)
-
-            reclamo_i = reclamos(title, description, keywords, itemreview,
-                                 summary, date_reclamo, ip_info, state_rec,
-                                 reclamo, ip_info, visitas, campo_empresa, empresa)
-
-            
-            writeJson(reclamo_i, path_out_csv)
-
+    with open(file_urls) as f:
+        lines = loadNlines(f, n_batch)
+        rec_batch = []
+        for line in lines:
+            line = line.replace('"', '')
+            url = line.replace('\n','')
+            soup = getURL(url)
+            reclamoi = proReclamo(soup, url)
+            if reclamoi != None: rec_batch.append(reclamoi) 
+        writeNjson(rec_batch, path_out)
     print('\n urls recorridas con éxito \n')
 
 
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import urllib.request
 import re
 import csv
 import json
+from itertools import islice
+from reclamos import *
 
 
 path_err = '../data/errores.log'
@@ -145,7 +147,49 @@ def write_error(error, path_file = path_err ):
         f.write(error + '\n')
 
 
-def writeJson(reclamo, path_out): 
-    data = rec2json(reclamo)
-    with open(path_out, 'a') as outfile:
-        json.dump(data, outfile)
+def loadNlines(file_opened, N):
+    return [x.strip() for x in islice(file_opened, N)]
+
+
+def writeNjson(reclamos, path_out):
+    with open(path_out, "a") as outfile:
+        for rec in reclamos:
+            data = rec2json(rec)
+            json.dump(data, outfile)
+            outfile.write('\n')
+
+
+def getURL(url):
+    try:
+        resp = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(resp, 'html.parser')
+    except urllib.error.HTTPError as err:
+        error = 'Error {} , url {}'.format(err.code, url)
+        write_error(error)
+        soup = None
+    except ValueError:
+        error = 'Error: Invalid URL {}'.format(url)
+        write_error(error)
+        soup = None
+    return soup
+
+
+def proReclamo(soup, url):
+    if soup != None:
+        title = getTitle(soup, url)
+        description = getDescription(soup, url)
+        keywords = getKeywords(soup, url)
+        itemreview = getItemreview(soup, url)
+        summary = getSummary(soup, url)
+        date_reclamo = getDate_reclamo(soup, url)
+        #date_consulta = getDate_consulta(soup, url)
+        ip_info = getIP_info(soup, url)
+        state_rec = getState_rec(soup, url)
+        reclamo = getReclamo(soup, url)
+        ip_user = getIP_user(soup, url)
+        visitas = getVisitas(soup, url)
+        campo_empresa = getCampoEmpresa(soup, url)
+        empresa = getEmpresa(soup, url)
+        reclamo_i = reclamos(title, description, keywords, itemreview, summary, date_reclamo, ip_info, state_rec, reclamo, ip_info, visitas, campo_empresa, empresa)
+        return reclamo_i
+    return None

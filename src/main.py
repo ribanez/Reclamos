17d962b2d485c6ecc17ac7822b920792b1d63307
Roberto A. Ibanez <33676489+ribanez@users.noreply.github.com>
2018-01-08 12:46:35 -0300

    update main.py

--- src/main.py
+++ src/main.py
@@ -48,43 +48,6 @@ def main_html2json():
     print('Base de datos leida con éxito!')
 
 
-def main_html2db():
-    data_base_exist = False
-    batch_size = 512
-    DB_HOST = 'localhost'
-    DB_USER = 'ribanez'
-    DB_PASSWORD = 'abc123'
-    DB_NAME = 'RECLAMOS_RAW'
-
-
-    cursor = dbconnect(host="localhost",
-                     user="ribanez",
-                     passwd="abc123",
-                     db="RECLAMOS")
-
-    con = mdb.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    cursor2 = con.cursor()
-
-    if not data_base_exist: create_sql(cursor2)
-
-    count = 309042 # cursor.execute(SELECT COUNT(*) FROM html)
-    for offset in range(0, count, batch_size):
-        
-        cursor.execute("SELECT url, html FROM html LIMIT %s OFFSET %s",(batch_size, offset))
-
-
-        for row in cursor.fetchall():
-            url = row[0]
-            html = row[1]
-            reclamoi = proReclamo(html, url)
-
-            #if reclamoi != None:
-                #populate_sql(cursor2, rec, url)
-
-        print('Se han leido {}/{}'.format(offset+batch_size,count))
-    print('Base de datos leida con éxito!')
-
-
 def printSchemaTable(cursor):
     cursor.execute("desc html")
     print([column[0] for column in cursor.fetchall()])
